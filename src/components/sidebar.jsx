e6dff5fa2db6f889a4c929dd24c433e6eb9460de
wino6687 <wino6687@colorado.edu>
2020-04-04 09:26:04 -0600

    remove lve link import

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
 
 export default class Sidebar extends Component {
   render() {
@@ -26,7 +25,6 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                {/*<li><Link to="/blog">Blog</Link></li>*/}
                 <li><a href="https://www.facebook.com/will.norris.35" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                 <li><a href="https://twitter.com/wino6687" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
                 <li><a href="https://www.instagram.com/willhikes/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
