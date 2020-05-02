b227bc9a30b18d6e09612e9681a3c9466be9c319
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-02 21:31:40 +0000

    Deploy wino6687/will-portfolio to github.com/wino6687/will-portfolio.git:gh-pages

---
+++ precache-manifest.61b099ce8d1ce92a0679d88d32723f57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d7bbf75ab46f4c6a47b",
+    "url": "/static/css/main.0949a9c6.chunk.css"
+  },
+  {
+    "revision": "4d7bbf75ab46f4c6a47b",
+    "url": "/static/js/main.4d7bbf75.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "841ce340d477bc6d90df03e021ffff63",
+    "url": "/index.html"
+  }
+];
